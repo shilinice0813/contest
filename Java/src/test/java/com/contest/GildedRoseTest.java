10ec4a874fee98941b988b47f0719d4bec8e9097
shili <shilinb@outlook.com>
2020-12-29 17:47:36 +0800

    1. 测试需求分析

--- Java/src/test/java/com/contest/GildedRoseTest.java
+++ Java/src/test/java/com/contest/GildedRoseTest.java
@@ -4,16 +4,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * 测试需求分析：
+ * 1. 普通商品销售期和品质每天变化验证；
+ * 2. “Aged Brie”的销售期和品质变化验证；
+ * 3. “Sulfuras”的销售期和品质变化验证；
+ * 4. “Backstage passes”的销售期和品质变化验证；
+ * 5. “Quality”的范围验证；
+ */
 public class GildedRoseTest {
 
-    @Test
-    public void should_fix_me() {
-        Item[] items = new Item[] { new Item("normal", 10, 20) };
-        GildedRose gildedRose = new GildedRose(items);
-        
-        gildedRose.updateQuality();
-
-        assertEquals(9, (gildedRose.items[0]).sellIn);
-        assertEquals(19, gildedRose.items[0].quality);
-    }
 }
