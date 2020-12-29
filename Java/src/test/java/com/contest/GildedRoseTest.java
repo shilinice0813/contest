afa428a1d06f4c3303aa93315226fb47d4c89d0a
shili <shilinb@outlook.com>
2020-12-29 18:00:52 +0800

    普通商品销售期和品质每天变化测试用例接口函数

--- Java/src/test/java/com/contest/GildedRoseTest.java
+++ Java/src/test/java/com/contest/GildedRoseTest.java
@@ -13,5 +13,14 @@ import static org.junit.Assert.assertEquals;
  * 5. “Quality”的范围验证；
  */
 public class GildedRoseTest {
+    //1.1 普通商品在销售期内，商品销售期和品质每天减1
+    @Test
+    public void Given_CommonGoods_When_InSellIn_Then_EveryDayLess1() {
 
+    }
+    //1.2 普通商品在销售期外，商品销售期每天减1，品质减2
+    @Test
+    public void Given_CommonGoods_When_OutSellIn_Then_EveryDaySellInLess1QualityLess2() {
+
+    }
 }
