6e1f52c364dc56ecdbd4b8bb8bcaf900c768a303
shili <shilinb@outlook.com>
2020-12-30 16:50:49 +0800

    普通商品质量不为负值验证

--- Java/src/test/java/com/contest/GildedRoseTest.java
+++ Java/src/test/java/com/contest/GildedRoseTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import java.text.ParseException;
 
-import static org.junit.Assert.assertEquals;
 
 /**
  * 测试需求分析：
@@ -16,12 +15,12 @@ import static org.junit.Assert.assertEquals;
  * 5. “Quality”的范围验证；
  */
 public class GildedRoseTest {
+    Item[] items=new Item[5];
     //1.1 普通商品在销售期内，商品销售期和品质每天减1
     @Test
     public void Given_CommonGoods_When_InSellIn_Then_EveryDayLess1() throws ParseException {
         //初始化商品名称，销售期，品质和上架日期
         Item item=new Item("Apple",20,10,"20201201");
-        Item[] items=new Item[5];
         items[0]=item;
         GildedRose gildedRose=new GildedRose(items);
         //设置当前日期为下一天
@@ -36,7 +35,6 @@ public class GildedRoseTest {
     public void Given_CommonGoods_When_OutSellIn_Then_EveryDaySellInLess1QualityLess2() throws ParseException{
         //初始化商品名称，销售期，品质和上架日期
         Item item=new Item("Apple",0,10,"20201201");
-        Item[] items=new Item[5];
         items[0]=item;
         GildedRose gildedRose=new GildedRose(items);
         //设置当前日期为下一天
@@ -46,4 +44,19 @@ public class GildedRoseTest {
         Assert.assertEquals(8,item.quality);
         Assert.assertEquals("20201202",item.lastUpdate);
     }
+    //1.3 普通商品品质不会降为负值
+    @Test
+    public void Given_CommonGoods_When_QualityWillLess0_Then_QualityMoreOrEquals0() throws ParseException {
+        //初始化商品名称，销售期，品质和上架日期
+        Item item=new Item("Apple",0,1,"20201201");
+        items[0]=item;
+        GildedRose gildedRose=new GildedRose(items);
+        //设置当前日期为下一天
+        MyDate.setCurrentDate("20201202");
+        gildedRose.updateQuality();
+        Assert.assertEquals(0,item.sellIn);
+        Assert.assertEquals(0,item.quality);
+        Assert.assertEquals("20201202",item.lastUpdate);
+    }
+
 }
